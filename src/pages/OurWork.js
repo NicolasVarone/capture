c5d355cca0b67b5be36d2209f6d5bec0da7b54e1
nicolas varone <varonenico@gmail.com>
2021-01-04 15:21:57 +0100

    Movie details

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -14,21 +14,21 @@ const OurWork = () => {
       <Movie>
         <h2>The Athlete</h2>
         <div className="line"></div>
-        <Link>
+        <Link to="/work/the-athlete">
           <img src={athlete} alt="athlet im Ring" />
         </Link>
       </Movie>
       <Movie>
         <h2>The Racer</h2>
         <div className="line"></div>
-        <Link>
+        <Link to="/work/the-racer">
           <img src={theracer} alt="woman sitting on a car" />
         </Link>
       </Movie>
       <Movie>
         <h2>Good Times</h2>
         <div className="line"></div>
-        <Link>
+        <Link to="/work/good-times">
           <img src={goodtimes} alt="couple enjoying the landscape" />
         </Link>
       </Movie>
