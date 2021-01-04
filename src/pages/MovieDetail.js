b1adc4b7f8b3ac757aab8316b87eb976e8b990f0
nicolas varone <varonenico@gmail.com>
2021-01-04 20:46:30 +0100

    Page animation

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -5,6 +5,9 @@ import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 // External File
 import { MovieState } from "../movieState";
+// Animations
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
 
 const MovieDetail = () => {
   const history = useHistory();
@@ -21,7 +24,12 @@ const MovieDetail = () => {
   return (
     <div>
       {movie && (
-        <Details>
+        <Details
+          exit="exit"
+          variants={pageAnimation}
+          initial="hidden"
+          animate="show"
+        >
           <HeadLine>
             <h2>{movie.title}</h2>
             <img src={movie.mainImg} alt="movie" />
@@ -45,7 +53,7 @@ const MovieDetail = () => {
 };
 
 // Style
-const Details = styled.div`
+const Details = styled(motion.div)`
   color: white;
 `;
 
--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -7,10 +7,19 @@ import styled from "styled-components";
 import athlete from "../img/athlete-small.png";
 import theracer from "../img/theracer-small.png";
 import goodtimes from "../img/goodtimes-small.png";
+// Animations
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
 
 const OurWork = () => {
   return (
-    <Work>
+    <Work
+      style={{ backgroundColor: "#fff" }}
+      exit="exit"
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+    >
       <Movie>
         <h2>The Athlete</h2>
         <div className="line"></div>
@@ -37,7 +46,7 @@ const OurWork = () => {
 };
 
 // Style
-const Work = styled.div`
+const Work = styled(motion.div)`
   min-height: 100vh;
   overflow: hidden;
   padding: 5rem 10rem;
